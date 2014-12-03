2d0790b6ef3474712c3d3b8fe3f0539bd1214579
Duffmaster <mtduffy4@gmail.com>
2014-12-03 14:51:04 -0700

    Made life background transparent

--- src/Life.java
+++ src/Life.java
@@ -1,13 +1,11 @@
 import java.awt.Color;
-
 import greenfoot.Actor;
 import greenfoot.GreenfootImage;
-
 
 public class Life extends Actor {
 	static int lives = 3;
 	
 	public void act(){
-		this.setImage(new GreenfootImage("Lives : " +lives, 24, Color.WHITE,new Color(0,0,0)));
+		this.setImage(new GreenfootImage("Lives : " +lives, 24, Color.WHITE,new Color(0,0,0,0)));
 	}
 }
