05f87639ac2e4146a8e49b51eeea6be8c60d1e76
CodyClawson <acdclive100@yahoo.com>
2014-12-03 14:32:54 -0700

    Create ShipBullet.java
    ships bullet

---
+++ src/ShipBullet.java
@@ -0,0 +1,40 @@
+import greenfoot.Actor;
+import greenfoot.GreenfootImage;
+
+
+public class ShipBullet extends Actor{
+	Bullet b ;
+	public ShipBullet(){
+		this.setImage("Images/SpaceShip.png");
+		this.setRotation(270);
+	}
+
+	public void act(){
+		this.isAtEdge();
+		this.move(5);
+		this.hitsBullet();
+	}
+
+	private void isAtEdge(){
+		int worldX=getWorld().getWidth();
+		int worldY=getWorld().getHeight();
+		int thisX=getX();
+		int thisY=getY();
+
+
+		if(thisX<=0||thisY<=0||thisX>=worldX||thisY>=worldY){
+			this.getWorld().removeObject(this);
+		}
+
+	}
+	
+	public void hitsBullet(){
+		if(this.getY()>20){
+		Bullet b = (Bullet) this.getOneIntersectingObject(Bullet.class);
+		}
+		if(b != null ){
+			this.getWorld().removeObject(b);
+			this.getWorld().removeObject(this);
+		}
+	}
+}
