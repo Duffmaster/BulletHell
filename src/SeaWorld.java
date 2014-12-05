4a641f743c8afb1267ac23cbfe449d161baeb941
Duffmaster <mtduffy4@gmail.com>
2014-12-05 15:55:27 -0700

    Added new patterns

--- src/SeaWorld.java
+++ src/SeaWorld.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-
 import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
 import greenfoot.World;
@@ -11,19 +10,38 @@ public class SeaWorld extends World {
 	final static int CELLSIZE = 1;
 	final static GreenfootImage BG=new GreenfootImage("images/space.png");
 
+	final static Bullet testBullet01=new Bullet(2,135,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet02=new Bullet(2,225,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet03=new Bullet(2,315,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet04=new Bullet(2,0,new GreenfootImage("images/RedBullet.png"));
 	final static Bullet testBullet=new Bullet(2,45,new GreenfootImage("images/BlueBullet.png"));
 	final static Bullet testBullet1=new Bullet(2,135,new GreenfootImage("images/RedBullet.png"));
 	final static Bullet testBullet2=new Bullet(2,225,new GreenfootImage("images/BlueBullet.png"));
 	final static Bullet testBullet3=new Bullet(2,315,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet4=new Bullet(20,-1,45,2,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet5=new Bullet(20,-1,315,-2,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet6=new Bullet(20,-1,225,2,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet7=new Bullet(20,-1,135,-2,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet8=new Bullet(2,45,new GreenfootImage("images/BlueBullet.png"));
+	final static Bullet testBullet9=new Bullet(2,135,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet10=new Bullet(2,225,new GreenfootImage("images/BlueBullet.png"));
+	final static Bullet testBullet11=new Bullet(2,315,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet12=new Bullet(2,45,new GreenfootImage("images/BlueBullet.png"));
+	final static Bullet testBullet13=new Bullet(2,135,new GreenfootImage("images/RedBullet.png"));
+	final static Bullet testBullet14=new Bullet(2,225,new GreenfootImage("images/BlueBullet.png"));
+	final static Bullet testBullet15=new Bullet(2,315,new GreenfootImage("images/RedBullet.png"));
 
-	final static Bullet[] testBullets={testBullet, testBullet1, testBullet2, testBullet3};
+	final static Bullet[] testBullets={testBullet01, testBullet02, testBullet03, testBullet04};
+	final static Bullet[] testBullets1={testBullet, testBullet1, testBullet2, testBullet3};
+	final static Bullet[] testBullets2={testBullet4, testBullet5};
+	final static Bullet[] testBullets3={testBullet6, testBullet7};
+	
 
 	private Bullet ghostBullet=new Bullet(1,180,new GreenfootImage(1,1));
 	private Bullet ghostBullet2=new Bullet(1,180,new GreenfootImage(1,1));
 
-
 	private static int level;
-	private int nol = 3;
+	private int nol = 6;
 
 	private GameWorld world;
 
@@ -103,52 +121,43 @@ public class SeaWorld extends World {
 			break;
 		case 1:
 			SeaWorld.level = level;
-			Spawner testSpawn=new Spawner(1,0,1,60,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner testSpawn1=new Spawner(-1,0,1,60,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
+			Spawner Spawn1=new Spawner(1,90,25,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
+			Spawner Spawn2=new Spawner(1,270,25,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
 
-			addObject(testSpawn, 0, 100);
-			addObject(testSpawn1, 900, 100);
-
+			addObject(Spawn1, 860, 100);
+			addObject(Spawn2, 40, 650);
 			isMenu=false;
 			break;
 		case 2:
 			SeaWorld.level = level;
-			Spawner Spawn1=new Spawner(1,0,90,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn2=new Spawner(1,90,90,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn3=new Spawner(1,270,90,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn4=new Spawner(1,180,90,20,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
+			Spawner Spawn3=new Spawner(1,0,1,60,testBullets1,new GreenfootImage("images/SpawnerDefault.png"));
+			Spawner Spawn4=new Spawner(-1,0,1,60,testBullets1,new GreenfootImage("images/SpawnerDefault.png"));
 
-			addObject(Spawn1, 40, 100);
-			addObject(Spawn2, 860, 100);
-			addObject(Spawn3, 40, 650);
-			addObject(Spawn4, 860, 650);
+			addObject(Spawn3, 0, 100);
+			addObject(Spawn4, 900, 100);
 			isMenu=false;
 			break;
 		case 3: 
 			SeaWorld.level = level;
-			Spawner Spawn0=new Spawner(-1,215,90,40,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn5=new Spawner(1,135,90,40,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn6=new Spawner(-1,135,90,40,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
-			Spawner Spawn7=new Spawner(1,215,90,40,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
 			
-			addObject(Spawn0, 40, 100);
-			addObject(Spawn5, 860, 100);
-			addObject(Spawn6, 40, 650);
-			addObject(Spawn7, 860, 650);
-			
-			if(Spawn0.getX() >= WIDTH - 100){
-				Spawn0.setRotation(180);
-			}
-			if(Spawn6.getX() >= WIDTH - 100){
-				Spawn6.setRotation(45);
-			}
 			isMenu=false;
 			break;
 		case 4:
 			SeaWorld.level = level;
+			Spawner Spawn01=new Spawner(0,90,356,360,testBullets,new GreenfootImage("images/SpawnerDefault.png"));
+			Spawner Spawn7=new Spawner(1,0,30,testBullets2,new GreenfootImage("images/BlueBullet.png"));
+			Spawner Spawn8=new Spawner(1,180,30,testBullets3,new GreenfootImage("images/BlueBullet.png"));
+			
+			addObject(Spawn01, 450, 100);
+			addObject(Spawn7, 1, 375);
+			addObject(Spawn8, 899, 375);
 			isMenu=false;
 			break;
 		case 5:
+			SeaWorld.level = level;
+			isMenu=false;
+			break;
+		case 6:
 			SeaWorld.level = level;
 			isMenu=false;
 			break;
